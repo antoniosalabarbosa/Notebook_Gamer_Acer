06625a5d365eb6c072685b875091d9a7f6c5b022
Antonio Sala Barbosa <sala.antonio2020@gmail.com>
2022-05-15 17:18:00 -0300

    WP-7

--- js/index.js
+++ js/index.js
@@ -1,9 +1,31 @@
 const burguer = document.getElementById("burguer");
 const menu_res = document.querySelector("header.hd_nav nav");
 
-burguer.addEventListener("click", ()=>{
-    if(menu_res.style.right == '-100%')
-        menu_res.style.right = '0%';
-    else
+function toggleNavRes(){
+    if(menu_res.style.right == '0%')
         menu_res.style.right = '-100%';
+    else
+        menu_res.style.right = '0%';
+}
+
+burguer.addEventListener("click", toggleNavRes);
+
+const links = document.querySelectorAll("header.hd_nav nav a");
+const section = document.querySelectorAll("section");
+
+function jumpTo(id){
+    let url = location.href;
+    location.replace("#"+id);
+    history.replaceState(null, null, url);
+    
+    window.scrollBy({
+        top: -110,
+        behavior: 'smooth',
+    });
+}
+
+links.forEach( (e) => {
+    e.addEventListener("click", ()=> {
+        jumpTo(e.textContent)
+    });
 });
